43d4ae25a9f7dac9cd5a7196056f4cd05e8d4754
Urvesh <urveshpatel94@yahoo.com>
2015-11-22 22:31:03 -0500

    changed user/password to what it was before

--- config/pool.js
+++ config/pool.js
@@ -5,8 +5,8 @@ var mysql = require('mysql');
 
 var pool = mysql.createPool({
     host: 'localhost',
-    user: 'root',
-    password: 'root',
+    user: 'user1',
+    password: 'password',
     database: 'is421'
 });
 
